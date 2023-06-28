27f51b6986988dee5141f1d24d07de1da4ad8918
Nehal <mdnehaladil@gmail.com>
2023-06-28 18:30:59 +0530

    fixed auth issue

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,75 +1,67 @@
-const passport = require('passport');
+const passport = require("passport");
 
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 
-const Employee = require('../models/employee');
+const Employee = require("../models/employee");
 
-
-// authentication using passport
-passport.use(new LocalStrategy({
-        usernameField: 'email'
+//authentication using passport
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
     },
-    function(email, password, done){
-        // find a user and establish the identity
-        Employee.findOne({email: email}, function(err, user)  {
-            if (err){
-                console.log('Error in finding user --> Passport');
-                return done(err);
-            }
+    function (email, password, done) {
+      //find a user and establish the identity
+      User.findOne({ email: email })
+        .then((user) => {
+          if (!user || user.password != password) {
+            console.log("Invalid Username/Password");
+            return done(null, false);
+          }
 
-            if (!user || user.password != password){
-                console.log('Invalid Username/Password');
-                return done(null, false);
-            }
-
-            return done(null, user);
+          return done(null, user);
+        })
+        .catch((err) => {
+          console.log("Error in finding User");
+          return done(err);
         });
     }
+  )
+);
 
-
-));
-
-
-// serializing the user to decide which key is to be kept in the cookies
-passport.serializeUser(function(user, done){
-    done(null, user.id);
+//serializing the user to decide which keys is to be kept in the cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
 
-
-
-// deserializing the user from the key in the cookies
-passport.deserializeUser(function(id, done){
-    Employee.findById(id, function(err, user){
-        if(err){
-            console.log('Error in finding user --> Passport');
-            return done(err);
-        }
-
-        return done(null, user);
+//deserializing the user from the key in the cookies
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then((user) => {
+      return done(null, user);
+    })
+    .catch((err) => {
+      console.log("Erro in finding user");
+      return done(err);
     });
 });
 
+//check if the user is authenticated
+passport.checkAuthentication = function (req, res, next) {
+  // if the user is signed in, then pass on the next function(controller's action)
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // if the user is  not signed in
+  return res.redirect("/users/sign-in");
+};
 
-// check if the user is authenticated
-passport.checkAuthentication = function(req, res, next){
-    // if the user is signed in, then pass on the request to the next function(controller's action)
-    if (req.isAuthenticated()){
-        return next();
-    }
-
-    // if the user is not signed in
-    return res.redirect('/emp/sign-in');
-}
-
-passport.setAuthenticatedUser = function(req, res, next){
-    if (req.isAuthenticated()){
-        // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
-        res.locals.user = req.user;
-    }
-
-    next();
-}
-
-
+passport.setAuthenticatedUser = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
+    res.locals.user = req.user;
+  }
+  next();
+};
 
 module.exports = passport;
--- index.js
+++ index.js
@@ -1,11 +1,63 @@
+const { setEngine } = require("crypto");
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+const passport = require("passport");
+const passportJWT = require("./config/passport-jwt-strategy");
+app.use(expressLayouts);
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
+app.use(express.urlencoded());
+
+app.use(express.static("./assets"));
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use(cookieParser());
+
+//mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    // TODO change the secret before deployement in production  mode
+    name: "placement",
+    secret: "secret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create(
+      {
+        mongoUrl: "mongodb://localhost/placement_development",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup ok");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use("/", require("./routes"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
-    console.log("Error", err);
+    console.log("err in runnig the server", err);
   }
 
-  console.log(`Server is running on port: ${port}`);
+  console.log("running on port", port);
 });
